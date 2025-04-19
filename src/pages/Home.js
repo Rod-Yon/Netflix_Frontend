42b0ef1a25143da2644b4ef6096d312a3dcced1c
shani-gal-or <shangalor@gmail.com>
2025-04-19 13:59:27 +0300

    doing the billboard component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 // pages/HomePage.jsx
 import React from "react";
 import Header from "../components/header/Header"
+import Billboard from "../components/billboard/Billboard";
 import Footer from "../components/footer_homepage/Footer_Homepage"
+import { Box } from "@mui/material";
 
 export default function HomePage() {
   
 
   return (
-    <div>
-      <Header/>
-      <Footer/>
-    </div>
+    <Box sx={{ position: "relative" }}>
+      <Header />
+      <Billboard />
+      <Footer />
+    </Box>
   );
 }
