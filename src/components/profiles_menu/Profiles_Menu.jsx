bfe133ef138b08b054bc2afc9e3915848a49556a
Novakovskiy Rodion <novakovskiy.ra@outlook.com>
2025-04-23 20:59:10 +0300

    Connection of "Profiles Menu" to the backend

--- src/components/profiles_menu/Profiles_Menu.jsx
+++ src/components/profiles_menu/Profiles_Menu.jsx
@@ -1,37 +1,147 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 
 import './Profiles_Menu.css'
 
+const api_url = 'http://localhost:8080';
+
 export default function Profile() {
 
-    let profiles = [
-        {
-            'name': 'Jennifer',
-            'avatar': '01'
-        },
-        {
-            'name': 'Bill',
-            'avatar': '05'
-        },
-        {
-            'name': 'Alice',
-            'avatar': '06'
-        },
-        {
-            'name': 'James',
-            'avatar': '15'
-        },
-    ];
+    const [profiles, Set_New_Profiles] = useState([]);
+    const [name_change_index, Set_Name_Change_Index] = useState(null);
+    const [new_name, Set_New_Name] = useState('');
+    const [error_element, Set_Error_Element] = useState('');
 
-    if (profiles.length < 5) {
-        profiles.push(
-            {
-                'name': 'Add Profile',
-                'avatar': '00'
+    const navigate = useNavigate();
+
+    const get_token = () => {
+
+        const local_token = localStorage.getItem('token');
+        const cookie_token = document.cookie.match(new RegExp('(^| )token=([^;]+)'));
+
+        const token = local_token || (cookie_token && cookie_token[2]);
+
+        if (!token) return null;
+        return token;
+    };
+
+    const get_profiles = async () => {
+
+        const token = get_token();
+
+        try {
+            const decoded = jwtDecode(token);
+            const username = decoded.username;
+
+            const response = await fetch(`${api_url}/profiles/${username}`);
+            if (!response.ok) throw new Error('Failed to fetch profiles');
+
+            const profiles = await response.json();
+            update_profiles(profiles);
+            return profiles;
+
+        } catch (error) {
+            alert(`Error fetching profiles: ${error}`);
+            return null;
+        }
+    };
+
+    const create_new_profile = async (index) => {
+
+        if (profiles[index].avatar != '00') {
+            redirect_page(profiles[index].profile_id);
+
+        } else {
+
+            Set_Error_Element('');
+
+            const token = get_token();
+
+            try {
+                const decoded = jwtDecode(token);
+                const username = decoded.username;
+
+                const response = await fetch(`${api_url}/profiles/${username}`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    credentials: 'include',
+                    body: JSON.stringify({ username }),
+                });
+
+                if (!response.ok) throw new Error('Failed to create new profile');
+
+                const profiles = await response.json();
+                update_profiles(profiles);
+                return profiles;
+
+            } catch (error) {
+                Set_Error_Element(error);
+                return null;
             }
-        );
-    }
+        }
+    };
 
+    const redirect_page = (profile_id) => {
+
+        navigate(`/home/${profile_id}`);
+    };
+
+    const change_name = (index) => {
+
+        if (profiles[index].name != 'Add Profile') {
+
+            Set_Error_Element('');
+            Set_Name_Change_Index(index);
+            Set_New_Name(profiles[index].name);
+        }
+    };
+
+    const new_name_submit = async (index) => {
+
+        const profile_id = profiles[index].profile_id;
+
+        try {
+            const response = await fetch(`${api_url}/profiles`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ profile_id, new_name })
+            });
+
+            if (!response.ok) throw new Error('Name Change Error');
+
+            update_profiles();
+
+        } catch (error) {
+
+            Set_Error_Element(error);
+        }
+
+        Set_Name_Change_Index(null);
+    };
+
+    const update_profiles = async (profiles = null) => {
+
+        let temp_profiles;
+
+        if (profiles) {
+            temp_profiles = profiles;
+        } else {
+            temp_profiles = await get_profiles();
+        }
+
+        if ((temp_profiles.length < 5) && (temp_profiles[temp_profiles.length - 1].name != 'Add Profile')) {
+            temp_profiles.push(
+                {
+                    'name': 'Add Profile',
+                    'avatar': '00'
+                }
+            );
+            Set_New_Profiles(temp_profiles);
+        }
+    };
+
+    useEffect(() => { update_profiles(); }, []);
 
     return (
         <div className='profiles_background'>
@@ -42,12 +152,25 @@ export default function Profile() {
                         {
                             profiles.map((person, index) => (
                                 <div className='profiles_card' key={index}>
-                                    <img src={`/assets/avatars/${person.avatar}.png`} />
-                                    <div className='profiles_name'>{person.name}</div>
+                                    <img className='profiles_avatar' src={`/assets/avatars/${person.avatar}.png`} alt='Avatar' onClick={() => create_new_profile(index)} />
+                                    {name_change_index === index ? (
+                                        <input
+                                            value={new_name}
+                                            onChange={(name) => { Set_New_Name(name.target.value) }}
+                                            onBlur={() => Set_Name_Change_Index(null)}
+                                            onKeyDown={(event) => {
+                                                if (event.key === 'Enter') {
+                                                    new_name_submit(index);
+                                                }
+                                            }}
+                                            autoFocus
+                                            className='profiles_input' />) : (
+                                        <div className='profiles_name' onClick={() => change_name(index)} >{person.name}</div>)}
                                 </div>
                             ))
                         }
                     </div>
+                    {Boolean(error_element) && <div className='error_element'>{error_element}</div>}
                 </div>
             </div>
         </div>
